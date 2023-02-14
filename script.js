0edca84d368953f01108b65478cb626685b30495
Joey Pettit <pettitbass@gmail.com>
2023-02-13 19:33:27 -0600

    starting number and list length in form can update list

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // global variables
 let defaultValueOne = "SCHEELS";
 let defaultValueTwo = ".COM";
+let defaultStartingNumber = 1;
+let defaultListLength = 100;
 let currentValueOne = defaultValueOne;
 let currentValueTwo = defaultValueTwo;
 let startingNumber = 1;
-let endingNumber = 100;
+let listLength = 100;
 let listArray = [];
 
 // select needed DOM nodes
@@ -14,26 +16,46 @@ const overlay = document.querySelector("#overlay");
 const accordion = document.querySelector("#accordion");
 const multipleOfThreeIn = document.querySelector("#multiple-3-input");
 const multipleOfFiveIn = document.querySelector("#multiple-5-input");
+const startingNumberIn = document.querySelector("#starting-number-input");
+const listLengthIn = document.querySelector("#list-length-input");
 const editForm = document.querySelector("#edit-form");
 
 // prepopulate form with default values
 multipleOfThreeIn.value = defaultValueOne;
 multipleOfFiveIn.value = defaultValueTwo;
+startingNumberIn.value = defaultStartingNumber;
+listLengthIn.value = defaultListLength;
 
 // create list and append to DOM
 generateNumberedArray();
 appendList();
 
+// ~~~ Event Listeners ~~~
+
+// accordion display toggle click listener
+displayFormButton.addEventListener("click", toggleDisplayForm);
+
+// click listener for overlay
+overlay.addEventListener("click", toggleDisplayForm);
+
+// click listener for UPDATE button
+editForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  updateCurrentFormValues();
+  generateNumberedArray();
+  appendList();
+  toggleDisplayForm();
+  console.log("current list", listLength);
+});
+
+// ~~~ Functions ~~~
+
 // function to generate the numbered list with added values from the form
 function generateNumberedArray() {
-  // grab current values from Form element
-  let currentValueOne = multipleOfThreeIn.value;
-  let currentValueTwo = multipleOfFiveIn.value;
-
   // clear global array
   listArray = [];
 
-  for (let i = startingNumber; i <= endingNumber; i++) {
+  for (let i = startingNumber; i <= startingNumber + listLength - 1; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       listArray.push(String(currentValueOne + currentValueTwo));
     } else if (i % 3 === 0) {
@@ -66,31 +88,19 @@ function appendList() {
   }
 }
 
-// accordion display toggle click listener
-displayFormButton.addEventListener("click", function () {
-  this.classList.toggle("active");
+function toggleDisplayForm() {
   overlay.classList.toggle("active");
-
+  displayFormButton.classList.toggle("active");
   if (accordion.style.maxHeight) {
     accordion.style.maxHeight = null;
   } else {
     accordion.style.maxHeight = accordion.scrollHeight + "px";
   }
-});
+}
 
-// click listener for overlay
-overlay.addEventListener("click", function () {
-  this.classList.toggle("active");
-  displayFormButton.classList.toggle("active");
-  accordion.style.maxHeight = null;
-});
-
-// click listener for UPDATE button
-editForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  console.log("okay");
+function updateCurrentFormValues() {
   currentValueOne = multipleOfThreeIn.value;
   currentValueTwo = multipleOfFiveIn.value;
-  generateNumberedArray();
-  appendList();
-});
+  startingNumber = Number(startingNumberIn.value);
+  listLength = Number(listLengthIn.value);
+}
